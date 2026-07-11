4062aa2d12f1b9cd2a69889a3dd9f28d8dc532d9
u006E10011 <zaaaaazzzazzaz@gmail.com>
2026-07-11 20:16:35 +0800

    update README, text command start/help

--- lib/src/command/command.dart
+++ lib/src/command/command.dart
@@ -1,4 +1,6 @@
+import 'package:televerse/telegram.dart' show ParseMode;
 import 'package:televerse/televerse.dart';
+import 'dart:io';
 
 class Command {
   final Bot _bot;
@@ -12,10 +14,17 @@ class Command {
 
   Future<void> startCommandAsync(Context ctx) async {
     try {
-      await ctx.reply(
-        'Bot: ${_bot.botInfo.me!.firstName}/@${_bot.me.username}\n'
-        'Developer: он .rar/@ryadevn\n',
-      );
+      await ctx.reply('''
+Позволяет сохранять медиа по «имени/фильтру» (изображения и стикеры) и затем отправлять по текстовому запросу.
+
+1. Фильтры создаются командой `/filter <filter_name>` в **ответ** на сообщение с:
+   - фото (сохраняется как `MediaType.image`)
+   - стикером (сохраняется как `MediaType.sticker`)
+2. Имя фильтра используется как ключ.
+3. Медиа сохраняется в `data/data_storage.json` (fileId + метаданные).
+4. Далее бот парсит текст и по **вхождению** имени фильтра в текст отправляет сохранённое медиа.
+5. Есть команды для управления фильтрами и просмотра списка.
+''', parseMode: ParseMode.markdown);
     } catch (e) {
       print('Error sending start message: $e');
     }
@@ -23,15 +32,17 @@ class Command {
 
   Future<void> helpCommandAsync(Context ctx) async {
     try {
-      await ctx.reply(
-        '/start - Информация о боте\n'
-        '/help - Помощь\n'
-        '/filter <filter_name> - Создать фильтр\n'
-        '/remove <filter_name> - Удалить фильтр\n'
-        '/edit <old_filter_name> <new_filter_name> - Изменить фильтр\n'
-        '<filter_name> - Поиск медиа по фильтру\n'
-        '/filters - Список фильтров',
-      );
+      await ctx.reply('''
+`/start` — Информация о боте
+`/help` — Список доступных команд
+`/filter <filter_name>` — Создать/задать фильтр, указав команду **в ответ на фото или стикер**
+`/filters [param]` — Список фильтров (порциями по 10, с inline-кнопками перелистывания)
+    `-image`, `-img`, `-i` — только изображениях
+    `-sticker`, `-stk`, `-s` — только стикеры
+    `-all`, `-a` — все фильтры
+`/remove <filter_name>` — Удалить фильтр
+`/edit <old_filter> <new_filter>` — Переименовать фильтр
+''', parseMode: ParseMode.markdown);
     } catch (e) {
       print('Error sending help message: $e');
       await ctx.reply('Ошибка при получении помощи');
